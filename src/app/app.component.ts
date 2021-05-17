e9a6804bc16ec6db34a2a7a88d3ad6f34a0110fc
Leah Shwartz <Leah21222@gmail.com>
2021-05-17 23:50:59 +0300

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,4 @@
 import { Component,Input } from '@angular/core';
-import {Pipe} from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -7,21 +6,12 @@ import {Pipe} from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 
-// @Pipe({
-//   name: "@gmail.com",
-//   pure: false,  
-// })
-
 export class AppComponent {
-  number: number = 248;
-  code = 'ILS';
-  date = new Date(2021,5,6);
-  email="Leah21222@gmail.com";
-  word="heLlO";
+ 
   constructor() { 
   }
-  title = 'Pipe';
-  
+  title = 'call';
+  show=0;
  
  
    
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,15 +3,22 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ShowWorkersComponent } from './show-workers/show-workers.component';
+import { ShowRoleComponent } from './show-role/show-role.component';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
     AppComponent,
+    ShowWorkersComponent,
+    ShowRoleComponent,
+
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule
+    FormsModule,
+    HttpClientModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
